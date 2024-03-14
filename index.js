a945695f7c580e988624076aad033e85c5b0a31a
ZaiManzano <gianmanzano123@gmail.com>
2024-03-14 16:01:30 +0800

    bingo

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello World.");
+
+generateRandomNumber(30);
+
+function generateRandomNumber(max) {
+    let randomNum = Math.floor(Math.random() * max) + 1;
+   
+
+    if(randomNum <= 15){
+       return console.log(`Random Number ${randomNum} is in B. 1 - 15`)
+    } else if (randomNum >=16 && randomNum <=30) {
+       return console.log(`Random Number ${randomNum} is in I. 16 - 30`)
+    } else if (randomNum >=31 && randomNum <=45) {
+        return console.log(`Random Number ${randomNum} is in N. 31 - 45`)
+    } else if (randomNum >=46 && randomNum <=60) {
+        return console.log(`Random Number ${randomNum} is in G. 46 - 60`)
+    } else if (randomNum >=61 && randomNum <=75) {
+        return console.log(`Random Number ${randomNum} is in O. 61 - 75`)
+    } 
+    else {
+       return console.log(`Random Number is invalid.`)
+    }  
+}
